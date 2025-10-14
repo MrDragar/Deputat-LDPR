b81962db7c9aa3b3f34c6e36bcec8f62e1e56acc
mrdragar <yshhenyaev@mail.ru>
2025-10-14 18:00:18 +0300

    change generator

--- tg_bot/src/handlers/join.py
+++ tg_bot/src/handlers/join.py
@@ -3,14 +3,13 @@ from typing import Any
 from aiogram import Router, types
 
 from src.services.user import check_user_is_available
-from src.database import get_db
-
+from src.database import get_db, get_db_sync
 
 router = Router()
 
 
 @router.chat_join_request()
 async def chat_join_request_handler(chat_join_request: types.ChatJoinRequest) -> Any:
-    if check_user_is_available(get_db(), chat_join_request.from_user.id):
+    if check_user_is_available(get_db_sync(), chat_join_request.from_user.id):
         return await chat_join_request.approve()
     await chat_join_request.decline()
