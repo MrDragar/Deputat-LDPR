02e292a05f1a41fafc106e70add169de6ce7b69b
mrdragar <yshhenyaev@mail.ru>
2025-11-01 13:32:20 +0300

    Change JWT lifetime

--- backend_auth/web/settings.py
+++ backend_auth/web/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'USER_ID_FIELD': 'user_id',
     'USER_ID_CLAIM': 'user_id',
     'ROTATE_REFRESH_TOKENS': False,
