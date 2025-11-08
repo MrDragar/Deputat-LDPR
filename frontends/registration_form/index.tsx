3382e73d2b4e3dc8f6037bd5792c114888f1e200
mrdragar <yshhenyaev@mail.ru>
2025-11-08 20:53:54 +0300

    Change intex.tsx for reg forms

--- frontends/registration_form/index.tsx
+++ frontends/registration_form/index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {BrowserRouter} from "react-router-dom";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,6 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter><App /></BrowserRouter>
+
   </React.StrictMode>
 );
