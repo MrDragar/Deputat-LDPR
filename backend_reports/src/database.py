5d29fd16a5c1e3cc98bf47144cd7aa75c18ee3d3
mrdragar <yshhenyaev@mail.ru>
2025-12-23 15:20:45 +0300

    Add db#2

---
+++ backend_reports/src/database.py
@@ -0,0 +1,33 @@
+import json
+import sqlite3
+from typing import Any, Dict
+
+
+class Database:
+    def __init__(self, db_path: str = "reports_db.sqlite3"):
+        self.conn = sqlite3.connect(db_path)
+        self.create_table()
+
+    def create_table(self):
+        """Создание таблицы если не существует"""
+        query = """
+        CREATE TABLE IF NOT EXISTS records (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            data TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        );
+
+        CREATE INDEX IF NOT EXISTS idx_user_id ON records(user_id);
+        CREATE INDEX IF NOT EXISTS idx_created_at ON records(created_at);
+        """
+        self.conn.executescript(query)
+        self.conn.commit()
+
+    def insert(self, user_id: int, data: Dict[str, Any]) -> int:
+        """Вставка записи для конкретного пользователя"""
+        query = "INSERT INTO records (user_id, data) VALUES (?, ?)"
+        cursor = self.conn.execute(query, (user_id, json.dumps(data)))
+        self.conn.commit()
+        return cursor.lastrowid
--- backend_reports/src/main.py
+++ backend_reports/src/main.py
@@ -6,12 +6,15 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.pdf_creater import generate_pdf_report
+from src.database import Database
 from src.model import InputData
 
 
 app = FastAPI()
 router = APIRouter(prefix='/api/reports')
 app.mount("/api/reports/media", StaticFiles(directory="media"), name="media")
+database = Database()
+database.create_table()
 
 app.add_middleware(
     CORSMiddleware,
@@ -33,6 +36,7 @@ async def create_pdf(input_data: InputData, request: Request):
     report_filename = f"report_{uuid.uuid4()}.pdf"
     report_filepath = os.path.join("media", report_filename)
     generate_pdf_report(input_data.data.dict(), report_filepath)
+    database.insert(input_data.user_id, input_data.data)
     return {"status": "Success", "message": f"{request.base_url}/api/reports/media/{report_filename}".replace(
         'https://', 'https://')}
 
