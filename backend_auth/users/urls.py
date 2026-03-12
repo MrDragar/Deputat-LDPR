53518aacb5103908364b83f3c82a5d8f30373587
mrdragar <yshhenyaev@mail.ru>
2026-03-12 04:06:32 +0300

    Add url pattern

--- backend_auth/users/urls.py
+++ backend_auth/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, re_path
-from .views import UserListAPIView, UserDetailAPIView
+from .views import UserListAPIView, UserDetailAPIView, UserUpdateAvailabilityAPIView
 
 urlpatterns = [
     path('/', UserListAPIView.as_view(), name='user-list'),
     re_path(r'^/(?P<user_id>-?\d+)/$', UserDetailAPIView.as_view(), name='user-detail'),
+    re_path(r'^/(?P<user_id>-?\d+)/update-availability$', UserUpdateAvailabilityAPIView.as_view(), name='user-detail'),
+
 ]
